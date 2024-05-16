a8087252df20f56cdb19233e733aab31411df653
danielhe27 <jdah49d@gmail.com>
2024-05-16 17:34:25 -0400

    mutations

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,7 +1,6 @@
 // Importa gql desde @apollo/client
 import { gql } from '@apollo/client';
 
-// Usa la función gql para definir la consulta me y expórtala
 export const GET_ME = gql`
   query me {
     me {
