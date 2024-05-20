9e64ddfa936e90af3f0ec6ceada343057b08509c
danielhe27 <jdah49d@gmail.com>
2024-05-19 21:11:27 -0400

    comments in config

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,8 @@
+//importing a mongoose library
 const mongoose = require('mongoose');
 
+//connecting to mongodb database using the connecting string from the.env file
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
+//exporting the connection object
 module.exports = mongoose.connection;
