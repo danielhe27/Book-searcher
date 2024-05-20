10e40f1d10d52cfdfdc3861dcc1367c6f75b314d
danielhe27 <jdah49d@gmail.com>
2024-05-20 18:18:47 -0400

    modification in server

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -12,11 +12,11 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: ({ req }) => authMiddleware({ req }),
+  persistedQueries: false //
 });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
@@ -35,7 +35,8 @@ const startApolloServer = async (typeDefs, resolvers) => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-    })
-  })
-  };
+    });
+  });
+};
+
 startApolloServer(typeDefs, resolvers);
