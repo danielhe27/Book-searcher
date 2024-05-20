88db106b399f9d388b4b29fd5396706aa9419ad9
danielhe27 <jdah49d@gmail.com>
2024-05-20 18:52:32 -0400

    dotenv

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+const fs = require('fs');
 const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -12,17 +13,24 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: ({ req }) => authMiddleware({ req }),
-  persistedQueries: false // or { cache: "bounded" } if you prefer
+  persistedQueries: false 
 });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Serve static files from the React app in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-  });
+  const distPath = path.join(__dirname, '../client/dist');
+  if (!fs.existsSync(distPath)) {
+    console.error('Dist directory does not exist:', distPath);
+  } else {
+    console.log('Serving static files from:', distPath);
+    app.use(express.static(distPath));
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(distPath, 'index.html'));
+    });
+  }
 } else {
   app.get('/', (req, res) => {
     res.send('API running');
@@ -33,7 +41,7 @@ if (process.env.NODE_ENV === 'production') {
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
-  
+
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
@@ -43,4 +51,3 @@ const startApolloServer = async (typeDefs, resolvers) => {
 };
 
 startApolloServer(typeDefs, resolvers);
-  
