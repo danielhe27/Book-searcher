7e8425593abb3cb6d3a708773b77824fab690092
danielhe27 <jdah49d@gmail.com>
2024-05-19 21:38:01 -0400

    mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,6 @@
 import { gql } from '@apollo/client';
 
+//  LOGIN_USER mutation to authenticate a user with email and password
 export const LOGIN_USER = gql`
   mutation loginUser($email: String!, $password: String!) {
     login(email: $email, password: $password) {
@@ -22,6 +23,7 @@ export const LOGIN_USER = gql`
   }
 `;
 
+// ADD_USER mutation to create a new user with username, email, and password
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
@@ -44,6 +46,7 @@ export const ADD_USER = gql`
   }
 `;
 
+// SAVE_BOOK mutation to save a book to a user's account
 export const SAVE_BOOK = gql`
   mutation saveBook($bookData: BookInput!) {
     saveBook(bookData: $bookData) {
@@ -63,6 +66,7 @@ export const SAVE_BOOK = gql`
   }
 `;
 
+// REMOVE_BOOK mutation to remove a book from a user's account
 export const REMOVE_BOOK = gql`
   mutation removeBook($bookId: String!) {
     removeBook(bookId: $bookId) {
