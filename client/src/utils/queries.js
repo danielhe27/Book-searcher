c4174c8707a3669b97be5e2ea9ded1d1d2e0f5b8
danielhe27 <jdah49d@gmail.com>
2024-05-19 21:42:51 -0400

    comment resolvers

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,6 +1,7 @@
 // Importa gql desde @apollo/client
 import { gql } from '@apollo/client';
 
+//  GET_ME query to fetch the current user's data
 export const GET_ME = gql`
   {
     me {
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,23 +2,28 @@ const { AuthenticationError } = require('apollo-server-express');
 const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
+// Define the resolvers for handling GraphQL queries and mutations
 const resolvers = {
   Query: {
+    // Resolver for the 'me' query to fetch the current user's data
     me: async (parent, args, context) => {
       if (context.user) {
-          const userData = await User.findOne({ _id: context.user._id })
-          .select('-__v -password')
-          return userData;
+        // Find the user by their ID, excluding version and password fields
+        const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+        return userData;
       }
+
       throw new AuthenticationError('Not logged in');
-  }
-},
+    }
+  },
   Mutation: {
+    // Resolver for the 'addUser' mutation to register a new user
     addUser: async (parent, args) => {
       const user = await User.create(args);
       const token = signToken(user);
       return { token, user };
     },
+    // Resolver for the 'login' mutation to authenticate a user
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
 
@@ -35,6 +40,7 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
+    // Resolver for the 'saveBook' mutation to save a book to the user's list
     saveBook: async (parent, { bookData }, context) => {
       if (context.user) {
         const updatedUser = await User.findByIdAndUpdate(
@@ -46,18 +52,20 @@ const resolvers = {
       }
       throw new AuthenticationError('You need to be logged in!');
     },
+    // Resolver for the 'removeBook' mutation to remove a book from the user's list
     removeBook: async (parent, { bookId }, context) => {
       if (context.user) {
-          const updatedUser = await User.findOneAndUpdate(
-              {_id: context.user._id},
-              { $pull: { savedBooks: { bookId: bookId } } },
-              { new: true }
-          )
-          return updatedUser;
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId: bookId } } },
+          { new: true }
+        );
+        return updatedUser;
       }
       throw new AuthenticationError('You need to be logged in!');
     },
   },
 };
+
 
 module.exports = resolvers;
